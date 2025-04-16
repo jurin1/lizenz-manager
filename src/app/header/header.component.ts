95e931e877abd6cb379da5e3b89c838d33be4c2a
Juri <neizcon@gmail.com>
2025-04-16 13:13:50 +0200

    designe update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,21 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule, MatIconModule],
 })
 export class HeaderComponent implements OnInit {
   isLoggedIn = false; // Beispielwert, sollte durch Authentifizierungslogik ersetzt werden
   isMenuOpen = false; // Zustand des Menüs
-  userLinks = [
-    { path: '/dashboard', label: 'Dashboard' },
-    { path: '/profile', label: 'Profil' },
-    { path: '/settings', label: 'Einstellungen' }
-  ];
+  orderCount = 3;
+  notificationCount = 10;
 
   constructor(private authService: AuthService, private router: Router) { }
 
